4364e8e0e93286f6f878855f51a323bfe0e72031
Thanos Eleftherakos <hello@bracket.gr>
2021-02-11 12:43:12 +0200

    remove index contents

--- index.php
+++ index.php
@@ -13,26 +13,3 @@
  * @subpackage Twenty_Twenty_One
  * @since 1.0.0
  */
-
-get_header();
-
-if ( have_posts() ) {
-
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
-
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-	}
-
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
-
-} else {
-
-	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
-
-}
-
-get_footer();
